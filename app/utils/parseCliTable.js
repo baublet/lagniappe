5ba2bbc323f816564f5e58e0af2fad51a3f2ed47
Ryan Poe <baublet@gmail.com>
2017-05-22 16:50:14 -0400

    Add global CLI table parser for JS as global function

---
+++ app/utils/parseCliTable.js
@@ -0,0 +1,19 @@
+export default function parseCliTable(output)
+{
+    const lineMatcher = /(\S+(?: \S+)*)/g
+    const lines = output.split("\n")
+    const rows = []
+    let headings = []
+    for(let i = 0; i < lines.length; i++) {
+        if (i == 0) {
+            headings = lines[i].match(lineMatcher)
+        } else {
+            const columns = lines[i].match(lineMatcher)
+            if(!columns || !columns.length) continue;
+            const row = {}
+            headings.forEach((heading, i) => row[heading] = columns[i])
+            rows.push(row)
+        }
+    }
+    return rows
+}
