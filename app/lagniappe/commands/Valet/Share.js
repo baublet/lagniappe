3b2534dbe18139de03ee9dcf23b91404d3a24896
Ryan Poe <baublet@gmail.com>
2017-07-18 14:36:43 -0400

    Add Valet Share command (non-functional at the moment)

---
+++ app/lagniappe/commands/Valet/Share.js
@@ -0,0 +1,20 @@
+import CommandProcess from 'lagniappe/commands/CommandProcess'
+import CommandWindow from 'lagniappe/commands/CommandWindow'
+
+export default class Share
+{
+
+    execute(site, cwd = './')
+    {
+        const command = [{
+            command: 'valet',
+            args: ['share'],
+            options: { cwd }
+        }]
+        const window = new CommandWindow('valet share')
+        const process = new CommandProcess(command, window.callback)
+        return process.execute()
+    }
+
+
+}
