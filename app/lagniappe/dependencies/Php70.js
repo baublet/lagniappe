0d262b1a84ce91da624e7089574b570e3a552028
Ryan Poe <baublet@gmail.com>
2017-07-20 09:50:13 -0400

    Add php70 dependency

---
+++ app/lagniappe/dependencies/Php70.js
@@ -0,0 +1,25 @@
+import Dependency from './Dependency'
+
+/**
+ * The base class for defining your application's dependencies and installation
+ * proceedures.
+ */
+export default class Php70 extends Dependency {
+
+    default()
+    {
+        this.dependencyName = 'Php'
+        this.dependencyLink = 'http://php.net/'
+        this.dependencyDocumentation = 'http://php.net/docs.php'
+        this.dependencyDescription = "PHP is a popular general-purpose scripting language that is especially suited to web development."
+        this.command = 'php -v'
+        this.expectedOutput = /PHP 7\.0/i
+        this.required = true
+    }
+
+    mac() {
+        this.installCommand = 'brew install php70'
+        this.uninstallCommand = 'brew uninstall php70'
+    }
+
+}
