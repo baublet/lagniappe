86214606174c8af9e95153fc0f1dfcda10eeb64b
Ryan Poe <baublet@gmail.com>
2017-07-20 10:30:56 -0400

    Fix MariaDB dependency expected output

--- app/lagniappe/dependencies/MariaDB.js
+++ app/lagniappe/dependencies/MariaDB.js
@@ -9,7 +9,7 @@ export default class MariaDB extends Dependency {
         this.dependencyDocumentation = 'https://mariadb.org/learn/'
         this.dependencyDescription = "MariaDB Server is one of the most popular database servers in the world. It’s made by the original developers of MySQL and guaranteed to stay open source. Notable users include Wikipedia, WordPress.com and Google."
         this.command = 'mysql --version'
-        this.expectedOutput = /MariaDB\.0/i
+        this.expectedOutput = /MariaDB/i
         this.required = true
     }
 
